073687427b407bbefb6ab24af6846ccde0729c9e
caio-melodev <caio16mla@gmail.com>
2025-05-03 15:42:34 -0300

    integração js e html em andamento funcional v1

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,23 @@ function searchCEP(cep) {
     return fetch(url)
         // convert to json
         .then(response => response.json())
-        .then(address => {
-            console.log(cep);
-            console.log("Street:", address.logradouro);
-            console.log("City:", address.localidade);
-        
+        .then(api => {
+            //console.log(cep);
+            document.getElementById("res-cep").textContent = api.cep;
+            document.getElementById("res-logradouro").textContent = api.logradouro;
+            document.getElementById("res-complemento").textContent = api.complemento;
+            document.getElementById("res-bairro").textContent = api.bairro;
+            document.getElementById("res-localidade").textContent = api.localidade;
+            document.getElementById("res-uf").textContent = api.uf;
+            document.getElementById("res-estado").textContent = api.estado;
+            document.getElementById("res-regiao").textContent = api.regiao;
+            document.getElementById("res-ibge").textContent = api.ibge;
+            document.getElementById("res-gia").textContent = api.gia;
+            document.getElementById("res-ddd").textContent = api.ddd;
+            document.getElementById("res-siafi").textContent = api.siafi;
+            //console.log("Street:", address.logradouro);
+            //console.log("City:", address.localidade);
+            
         })
         //error response
         .catch(error => {
